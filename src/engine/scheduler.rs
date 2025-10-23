3ab235c6005fe7167c04bef5bc9aa9ba4d677632
Viv <vivax3794@protonmail.com>
2025-10-23 20:01:26 +0200

    implement progress bar

--- src/engine/scheduler.rs
+++ src/engine/scheduler.rs
@@ -7,6 +7,7 @@ use tokio::sync::OnceCell;
 use super::RuntimeContext;
 use crate::engine::RuntimeError;
 use crate::engine::data_model::{Data, Graph, NodeInstanceId, NodeStorage};
+use crate::tui::TuiSender;
 
 /// Executes the various nodes
 pub struct Scheduler {
@@ -22,12 +23,16 @@ pub struct Scheduler {
 
 impl Scheduler {
     /// Create a new scheduler to run the given graph
-    pub async fn new(nodes: NodeStorage, graph: Graph) -> Result<Self, RuntimeError> {
+    pub async fn new(
+        nodes: NodeStorage,
+        graph: Graph,
+        tui: TuiSender,
+    ) -> Result<Self, RuntimeError> {
         Ok(Self {
             data: vec![OnceCell::new(); graph.len()],
             nodes,
             graph,
-            context: Rc::new(RuntimeContext::new().await?),
+            context: Rc::new(RuntimeContext::new(tui).await?),
         })
     }
 
@@ -38,25 +43,29 @@ impl Scheduler {
 
     /// Retrieve a data from a node.
     /// If the node hasn't started starts running it, then awaits on the result.
-    pub async fn get_output(&self, node: NodeInstanceId) -> Result<&Data, RuntimeError> {
-        let Some(cell) = self.data.get(node.index()) else {
+    pub async fn get_output(&self, node_id: NodeInstanceId) -> Result<&Data, RuntimeError> {
+        let Some(cell) = self.data.get(node_id.index()) else {
             return Err(RuntimeError::internal("NodeInstanceId out of bounds"));
         };
 
         let res = cell
             .get_or_try_init(async || {
-                let node = self.graph.get(node);
+                let node = self.graph.get(node_id);
+                let _ = self.context.tui.send(crate::tui::TuiMessage::PendingNode);
+
                 futures_util::future::try_join_all(
                     node.phantom_inputs.iter().map(|id| self.get_output(*id)),
                 )
                 .await?;
-
                 let node_impl = self.nodes.get(node.kind);
-                log::debug!("Executing node {}", node.kind.index());
-                node_impl.execute(self, &node.inputs).await
+
+                log::debug!("Executing node {node_id:?}");
+                let res = node_impl.execute(self, &node.inputs).await;
+                let _ = self.context.tui.send(crate::tui::TuiMessage::NodeFinished);
+                res
             })
             .await;
-        log::trace!("Got output of node {node:?}: {res:?}");
+        log::trace!("Got output of node {node_id:?}: {res:?}");
         res
     }
 }
