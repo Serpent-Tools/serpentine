04a30218ae28ba09316451ea6b23df9721453d4f
Viv <vivax3794@protonmail.com>
2025-12-01 09:30:05 +0100

    setup nix flake for serpentine

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Rust dev environment";
+  description = "Powerful simplistic workflow runner.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -16,24 +16,43 @@
       nixpkgs,
       fenix,
       flake-utils,
-      nur,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ nur.overlays.default ];
         };
       in
       {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "serpentine";
+          version = cargoToml.package.version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          meta = {
+            description = "Powerful simplistic workflow runner.";
+            license = pkgs.lib.licenses.mit;
+            mainProgram = "serpentine";
+            homepage = "https://github.com/Serpent-Tools/serpentine";
+            maintainers = [
+              {
+                github = "vivax3794";
+                email = "vivax3794@protonmail.com";
+                name = "Viv";
+              }
+            ];
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             (fenix.packages.${system}.latest.withComponents [
               "cargo"
               "clippy"
               "rustc"
-
               "rust-src"
               "rustfmt"
             ])
@@ -43,4 +62,3 @@
       }
     );
 }
-
